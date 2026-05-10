e21aa120568e5c0dccad84d12bc28b84a57b583a
Shubham Giri <imshubhamgiri@gmail.com>
2026-05-10 13:48:54 +0530

    chore(config): add cors and setup watch script for dev

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import routes from "./routes/index.js";
 import logger from "./logger/logger.js";
 
 
 const app = express();
 
+app.use(cors());
 app.use(logger);
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "src", "views"));
