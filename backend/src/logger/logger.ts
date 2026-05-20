61e98a73d52f095ade078bbd6bae8094c8d55536
imshubhamgiri <skgiri569@gmail.com>
2026-05-20 23:19:54 +0530

    add duration for one complete req-res lifecycle

--- backend/src/logger/logger.ts
+++ backend/src/logger/logger.ts
@@ -4,9 +4,11 @@ import { Request, Response, NextFunction } from "express";
 export default function log(request: Request, response: Response, next: NextFunction): void {
   if (process.env.NODE_ENV === "development") {
     const timestamp = new Date().toISOString();
+    let StartTime = Date.now();
     // Use setImmediate to defer logging (non-blocking)
-    setImmediate(() => {
-      console.log(`[${timestamp}] ${request.method} ${request.url}`);
+   request.on("finish", () => {
+      const duration = Date.now() - StartTime;
+      console.log(`[${timestamp}] ${request.method} ${request.originalUrl} - ${response.statusCode} (${duration}ms)`);
     });
   }
   next();
