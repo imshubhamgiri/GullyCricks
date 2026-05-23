4d18bd692c6f0ada9c7077b8926d715a60729100
imshubhamgiri <skgiri569@gmail.com>
2026-05-23 22:29:15 +0530

    feat: add UI components index for centralized exports

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Card } from "./Card";
+export { LoadingState } from "./LoadingState";
+export { ErrorState } from "./ErrorState";
+export { InputField } from "./InputField";
+export { ErrorAlert } from "./ErrorAlert";
