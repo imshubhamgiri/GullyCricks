c1c92baea8fccc1c2bb74a7b7fd0b578eb13c533
imshubhamgiri <skgiri569@gmail.com>
2026-05-23 22:33:40 +0530

    feat: add dashboard components index for centralized exports

---
+++ frontend/src/components/dashboard/index.ts
@@ -0,0 +1,8 @@
+export { Scoreboard } from "./Scoreboard";
+export { MatchInfo } from "./MatchInfo";
+export { AdminControls } from "./AdminControls";
+export { ScoreCard } from "./ScoreCard";
+export { UserCard } from "./UserCard";
+export { UsersPanel } from "./UsersPanel";
+export { DashboardHeader } from "./DashboardHeader";
+export { BallHistory } from "./BallHistory";
